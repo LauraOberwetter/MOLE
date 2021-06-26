6b1291165f07c71b7629783d9d6dbf173b3597cf
emilyalv <emilyaalv@gmail.com>
2021-06-26 11:20:53 -0500

    updated from mongodb files to mysql files

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
-// module.exports = {
-//   Post: require("./post")
-// };
+const User = require('./User');
+const Project = require('./Project');
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Project };
