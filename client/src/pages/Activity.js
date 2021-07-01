41e741a82c3c76dc01e3f20b043fefcb93ce032f
emilyalv <emilyaalv@gmail.com>
2021-06-30 20:55:08 -0500

    quiz and static.js file for heroku

--- client/src/pages/Activity.js
+++ client/src/pages/Activity.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Quiz from "../components/Quiz";
 
 const Activity = () => {
     return (
-        <h1>add activiy details here !</h1>
+        <>
+        <h1>Module 1: Activity 1</h1>
+        <Quiz/>
+        </>
     )};
 
 export default Activity;
