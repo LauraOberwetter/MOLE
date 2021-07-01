799d3f0d4674d777433656cfc005ed6ecd0b8c92
Smylebryte <aliciahughes1446@gmail.com>
2021-06-30 21:12:28 -0500

    corrected module seeds

--- seeds/module-seeds.js
+++ seeds/module-seeds.js
@@ -3,27 +3,27 @@ const { Module } = require("../models");
 const moduleData = [
   {
     module_name: "Japanese 1",
-    due-date: 7/5/2021,
+    due_date: 7 / 5 / 2021,
     status: "In Progress",
   },
   {
     module_name: "Japanese 2",
-    due-date: 7/15/2021,
+    due_date: 7 / 15 / 2021,
     status: "Not Started",
   },
   {
     module_name: "Japanese 3",
-    due-date: 7/30/2021,
+    due_date: 7 / 30 / 2021,
     status: "In Progress",
   },
   {
     module_name: "Japanese 4",
-    due-date: 7/20/2021,
+    due_datee: 7 / 20 / 2021,
     status: "Not Started",
   },
   {
     module_name: "Japanese 5",
-    due-date: 6/30/2021,
+    due_date: 6 / 30 / 2021,
     status: "Completed",
   },
 ];
