ba842e217a24fbb3fb312100fec2d14d72d033ec
Smylebryte <aliciahughes1446@gmail.com>
2021-07-10 11:06:35 -0500

    fixed env issue

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,11 @@
 const sequelize = require("../config/connection");
 const { Language, Course, Module, Activity } = require("../models");
 
-// const studentSeedData = require("./studentSeedData.json");
 const languageSeedData = require("./languageSeedData.json");
 const courseSeedData = require("./courseSeedData.json");
 const moduleSeedData = require("./moduleSeedData.json");
 const activitySeedData = require("./activitySeedData.json");
 
-// const seedStudents = () => Student.bulkCreate(studentSeedData);
 const seedLanguages = () => Language.bulkCreate(languageSeedData);
 const seedCourses = () => Course.bulkCreate(courseSeedData);
 const seedModules = () => Module.bulkCreate(moduleSeedData);
@@ -33,33 +31,3 @@ const seedAll = async () => {
 };
 
 seedAll();
-
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
-
-//   const students = await Student.bulkCreate(studentSeedData);
-
-//   const modules = await Module.bulkCreate(moduleSeedData);
-
-//   process.exit(0);
-// };
-
-// seedDatabase();
-
-// const seedAll = async () => {
-//   await sequelize.sync({ force: true });
-//   console.log("\n----- DATABASE SYNCED -----\n");
-
-//   await seedGrades();
-//   console.log("\n----- GRADES SEEDED -----\n");
-
-//   await seedStudents();
-//   console.log("\n----- STUDENTS SEEDED -----\n");
-
-//   await seedModules();
-//   console.log("\n----- MODULES SEEDED -----\n");
-
-//   process.exit(0);
-// };
-
-// seedAll();
