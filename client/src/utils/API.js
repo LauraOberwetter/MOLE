a88e1819c9238d64d7c06cf607019167416196d7
Smylebryte <aliciahughes1446@gmail.com>
2021-07-14 21:31:36 -0500

    got modules to render to page

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,22 +1,20 @@
-//SAMPLE CODE
+import axios from "axios";
 
-// import axios from "axios";
-
-// export default {
-//   // Gets all posts
-//   getPosts: function() {
-//     return axios.get("/api/posts");
-//   },
-//   // Gets the post with the given id
-//   getPost: function(id) {
-//     return axios.get("/api/posts/" + id);
-//   },
-//   // Deletes the post with the given id
-//   deletePost: function(id) {
-//     return axios.delete("/api/posts/" + id);
-//   },
-//   // Saves a post to the database
-//   savePost: function(postData) {
-//     return axios.post("/api/posts", postData);
-//   }
-// };
+export default {
+  // Gets all modules
+  getModules: function () {
+    return axios.get("api/modules");
+  },
+  //   // Gets the post with the given id
+  //   getPost: function (id) {
+  //     return axios.get("/api/posts/" + id);
+  //   },
+  //   // Deletes the post with the given id
+  //   deletePost: function (id) {
+  //     return axios.delete("/api/posts/" + id);
+  //   },
+  //   // Saves a post to the database
+  //   savePost: function (postData) {
+  //     return axios.post("/api/posts", postData);
+  //   },
+};
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
 
@@ -8,12 +9,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turn on routes
+app.use(routes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// turn on routes
-app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build"));
+});
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
