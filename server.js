781cb128a7dec7eeb857cca695740ef76a98d23c
emilyalv <emilyaalv@gmail.com>
2021-07-17 13:49:50 -0500

    updated seed script in package.json

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// if (process.env.NODE_ENV === "production") {
-//   app.use('/static', express.static(path.join(__dirname, 'client/build')));
-// }
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./client/build"));
