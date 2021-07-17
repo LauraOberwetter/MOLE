a9c9ee3135def18cba385ee9b3f328a9254aa407
emilyalv <emilyaalv@gmail.com>
2021-07-17 11:43:34 -0500

    updating path to static assets

--- server.js
+++ server.js
@@ -12,12 +12,18 @@ app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')));
+
 }
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build"));
+ 
 });
 
 // turn on connection to db and server
