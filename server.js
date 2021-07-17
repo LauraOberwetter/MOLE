07a8eee9238941f6e3c33dd98f5d14cec89d6c17
emilyalv <emilyaalv@gmail.com>
2021-07-17 12:52:26 -0500

    removed build and node modules

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -23,9 +21,14 @@ if (process.env.NODE_ENV === "production") {
 //   res.sendFile(path.join(__dirname, "./client/build"));
 // });
 
+// turn on routes
+app.use(routes);
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}!`);
   });
 });
+
+
