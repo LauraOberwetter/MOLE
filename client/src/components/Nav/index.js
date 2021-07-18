8556a74a39907e414e02aa53e5649aaa5dbd1a1c
Laura Oberwetter <leoberwetter@gmail.com>
2021-07-17 20:06:40 -0500

    updated nav bar, changed favicon, hamburger menu not working yet

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,21 +1,46 @@
 import React from "react";
 import "./style.css";
 import { NavLink } from "react-router-dom";
+import { Nav, Bars, NavMenu, NavBtn, NavBtnLink } from "./navElements";
+import logo from "./logo.png";
 
-
-class Nav extends React.Component {
-
+class Navbar extends React.Component {
   render() {
     return (
-        <nav>
-          <NavLink to="/">Dashboard</NavLink>
-          <NavLink to="/login">Login</NavLink>
-          <NavLink to="/module">Module</NavLink>
-          <NavLink to="/activity">Activity</NavLink>
-        </nav>
+      <Nav>
+        {/* logo */}
+        <NavLink to="/" className="logo">
+          <img src={logo} alt="HVPT Logo" />
+        </NavLink>
+        {/* hamburger menu */}
+        <Bars />
+        {/* div for wrapping links */}
+        <NavMenu className='nav'>
+          <NavLink className="navLink" to="/">
+            Dashboard 
+          </NavLink>
+          <NavLink
+            className="navLink"
+            activeStyle={{ fontWeight: "bold" }}
+            to="/login">
+            Login 
+          </NavLink>
+          <NavLink
+            className="navLink"
+            activeStyle={{ fontWeight: "bold" }}
+            to="/module">
+            Module 
+          </NavLink>
+          <NavLink
+            className="navLink"
+            activeStyle={{ fontWeight: "bold" }}
+            to="/activity">
+            Activity 
+          </NavLink>
+        </NavMenu>
+      </Nav>
     );
   }
 }
 
-export default Nav;
-
+export default Navbar;
