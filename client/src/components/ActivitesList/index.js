4ff9b155f77d42ff8bcd165121cf958bfdc97f31
emilyalv <emilyaalv@gmail.com>
2021-07-19 18:47:44 -0500

    added comma back to package-lock

--- client/src/components/ActivitesList/index.js
+++ client/src/components/ActivitesList/index.js
@@ -1,13 +1,13 @@
-import React,{useEffect, useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-// import API from '../../utils/API';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import API from "../../utils/API";
 
 const useStyles = makeStyles({
   table: {
@@ -15,35 +15,35 @@ const useStyles = makeStyles({
   },
 });
 
-
 export default function ActivityTable() {
   const classes = useStyles();
 
-//Populate the Table with Data from an API
+  const [activities, setactivities] = useState([]);
+
+  //Populate the Table with Data from an API
   // const [ modules, setModules ] = useState([]);
 
   // useEffect(() => {
-    // const getModules = async () => {
-    //   try {
-    //     const moduleList = await API.search()
-    //     setModules(moduleList.data.results)
-    //   } catch (error) {
-    //     console.log(error)        
-    //   }
-    // }
-    // getModules();
+  // const getModules = async () => {
+  //   try {
+  //     const moduleList = await API.search()
+  //     setModules(moduleList.data.results)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+  // getModules();
   // }, [])
 
   function createData(module, status, due, user) {
     return { module, status, due, user };
   }
-  
-  const rows = [
-    createData('Activity 1', 'In Progress', '08/21/2021', 'Emily'),
-    createData('Activity 2', 'In Progress', '09/21/2021', 'Emily'),
-    createData('Activity 3', 'Not Started', '10/21/2021', 'Emily'),
-  ];
 
+  const rows = [
+    createData("Activity 1", "In Progress", "08/21/2021", "Emily"),
+    createData("Activity 2", "In Progress", "09/21/2021", "Emily"),
+    createData("Activity 3", "Not Started", "10/21/2021", "Emily"),
+  ];
 
   return (
     <TableContainer component={Paper}>
@@ -56,11 +56,11 @@ export default function ActivityTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.module}>
-              <TableCell align="right">{row.module}</TableCell>
+          {activities.map((row) => (
+            <TableRow key={row.activity}>
+              <TableCell align="right">{row.activity_name}</TableCell>
               <TableCell align="right">{row.status}</TableCell>
-              <TableCell align="right">{row.due}</TableCell>
+              <TableCell align="right">{row.due_date}</TableCell>
             </TableRow>
           ))}
         </TableBody>
