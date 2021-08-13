8d0f9a34ebc268924ad5bd94f016e47d860a97d9
Laura Oberwetter <leoberwetter@gmail.com>
2021-08-12 19:21:09 -0500

    consent and reg debugged, post error with reg

--- client/src/components/Consent/index.js
+++ client/src/components/Consent/index.js
@@ -1,100 +1,19 @@
 import React from "react";
+import {Button} from "../Button/Button.jsx";
+
+// NOT NEEDED, DELETE BEFORE PUBLISHING
 //import Button from "react-bootstrap/Button";
 //import React, { useState } from "react";
-import "./consent.css";
-import {Button} from "../Button/Button.jsx";
+//import "./consent.css";
 
 // should be able to import register component?
 //import Register from "../Register";
 
-export default function Consent() {
+export default ({setActionTakenOnConsent}) => {  
   return (
-    <div className="Consent">
-      <div className="content">
-        <h1 className="consentHead">Training Participant Consent Form</h1>
-        <div className="copyAndButtons">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed
-            augue lacus viverra vitae congue. Rhoncus mattis rhoncus urna neque
-            viverra justo. Nam at lectus urna duis. Morbi tincidunt augue
-            interdum velit euismod in pellentesque massa. Cras semper auctor
-            neque vitae tempus quam pellentesque nec nam. Pellentesque
-            adipiscing commodo elit at imperdiet dui accumsan sit. Vitae tortor
-            condimentum lacinia quis vel eros donec ac odio. Pellentesque nec
-            nam aliquam sem et tortor consequat id porta. Viverra nibh cras
-            pulvinar mattis nunc. Nunc sed augue lacus viverra vitae congue.
-            Dapibus ultrices in iaculis nunc sed augue lacus viverra. Sit amet
-            massa vitae tortor condimentum lacinia quis vel eros. Commodo sed
-            egestas egestas fringilla phasellus faucibus scelerisque eleifend.
-            Massa vitae tortor condimentum lacinia quis vel eros. Cursus metus
-            aliquam eleifend mi in nulla posuere. Placerat in egestas erat
-            imperdiet. Sapien eget mi proin sed libero enim sed faucibus. Turpis
-            massa sed elementum tempus egestas. Sit amet purus gravida quis
-            blandit turpis cursus in hac. Lectus quam id leo in vitae turpis
-            massa. Risus nec feugiat in fermentum posuere urna. Purus semper
-            eget duis at. Purus gravida quis blandit turpis cursus in hac
-            habitasse platea. Elit duis tristique sollicitudin nibh sit amet.
-            Vitae auctor eu augue ut lectus arcu. Malesuada pellentesque elit
-            eget gravida. Mi proin sed libero enim sed. Pharetra pharetra massa
-            massa ultricies mi quis hendrerit. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Nunc sed augue lacus viverra vitae
-            congue. Rhoncus mattis rhoncus urna neque viverra justo. Nam at
-            lectus urna duis. Morbi tincidunt augue interdum velit euismod in
-            pellentesque massa. Cras semper auctor neque vitae tempus quam
-            pellentesque nec nam. Pellentesque adipiscing commodo elit at
-            imperdiet dui accumsan sit. Vitae tortor condimentum lacinia quis
-            vel eros donec ac odio. Pellentesque nec nam aliquam sem et tortor
-            consequat id porta. Viverra nibh cras pulvinar mattis nunc. Nunc sed
-            augue lacus viverra vitae congue. Dapibus ultrices in iaculis nunc
-            sed augue lacus viverra. Sit amet massa vitae tortor condimentum
-            lacinia quis vel eros. Commodo sed egestas egestas fringilla
-            phasellus faucibus scelerisque eleifend. Massa vitae tortor
-            condimentum lacinia quis vel eros. Cursus metus aliquam eleifend mi
-            in nulla posuere. Placerat in egestas erat imperdiet. Sapien eget mi
-            proin sed libero enim sed faucibus. Turpis massa sed elementum
-            tempus egestas. Sit amet purus gravida quis blandit turpis cursus in
-            hac. Lectus quam id leo in vitae turpis massa. Risus nec feugiat in
-            fermentum posuere urna. Purus semper eget duis at. Purus gravida
-            quis blandit turpis cursus in hac habitasse platea. Elit duis
-            tristique sollicitudin nibh sit amet. Vitae auctor eu augue ut
-            lectus arcu. Malesuada pellentesque elit eget gravida. Mi proin sed
-            libero enim sed. Pharetra pharetra massa massa ultricies mi quis
-            hendrerit.
-          </p>
-          <Button
-            type="button"
-            onClick={(e) => {
-              e.preventDefault();
-              window.location.href = "https://ling.towson.edu/";
-            }}> 
-            AGREE</Button>
-
-          <Button
-            type="button"
-            onClick={(e) => {
-              e.preventDefault();
-              window.location.href = "https://ling.towson.edu/";
-            }} buttonStyle="btn--primary--solid">
-            DISAGREE</Button>
-
-          {/* endless loading when Register component is imported here */}
-          {/* <Register /> */}
-
-        </div>
-      </div>
-    </div>
-  );
-}
-
-
-
-/* export default function Consent({ accept, decline }) {
-  return (
-    <div className="content">
+    <div>
       <h1 className="consentHead">Training Participant Consent Form</h1>
-      <div className="copyAndButtons">
+      <div className="consent-copyAndButtons">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed
@@ -143,9 +62,27 @@ export default function Consent() {
           proin sed libero enim sed. Pharetra pharetra massa massa ultricies mi
           quis hendrerit.
         </p>
-        <div>
-          <Button onClick={accept}>Yes</Button> {""}
-          <Button onClick={decline}>No</Button> 
-              //</div>
-  //</div>
-  */
+        <Button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            setActionTakenOnConsent(true);
+            //window.location.href = "https://ling.towson.edu/";
+          }}>
+            AGREE</Button>
+
+        <Button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            setActionTakenOnConsent(true);
+            //window.location.href = "https://ling.towson.edu/";
+          }}buttonStyle="btn--primary--solid">
+          DISAGREE</Button>
+
+        {/* endless loading when Register component is imported here */}
+        {/* <Register /> */}
+      </div>
+    </div>
+  );
+}
--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+//import Form from "react-bootstrap/Form";
+//import Button from "react-bootstrap/Button";
+//import Row from "react-bootstrap/Row";
+//import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Consent from "../Consent";
-//import "./style.css";
+import FormForRegistration from "../FormForRegistration/FormForRegistration";
+import "./style.css";
 
 const Register = ({ changeScreen, setUserLogged, userLogged }) => {
   const [user, setUser] = useState({
@@ -20,227 +21,267 @@ const Register = ({ changeScreen, setUserLogged, userLogged }) => {
     password: "",
     opt_in: false,
   });
+  const [actionTakenOnConsent, setActionTakenOnConsent] = useState(false);
 
-  const [load, setLoad] = useState(0);
+  // const [load, setLoad] = useState(0);
 
-  // @INCOMPLETE remove this before final version
-  useEffect(() => {
-    axios
-      .get("http://localhost:3001/api/users")
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [load]);
-
-  const submitUser = (e) => {
-    e.preventDefault();
-    console.log(user);
-    axios
-      .post("http://localhost:3001/api/users", user)
-      .then((res) => {
-        setUser({
-          first_name: "",
-          last_name: "",
-          username: "",
-          email: "",
-          language: "",
-          course: "",
-          instructor: "",
-          password: "",
-          opt_in: false,
-        });
-        console.log(res);
-        localStorage.setItem("user", JSON.stringify(res.data.user.id));
-        userLogged === 0 ? setUserLogged(1) : setUserLogged(0);
-        load === 0 ? setLoad(1) : setLoad(0);
-        window.location.replace("https://ling.towson.edu/");
-      })
-      .catch((err) => {
-        console.log(err);
-        load === 0 ? setLoad(1) : setLoad(0);
-      });
-  };
-
-  const changeHandler = (e) => {
-    e.preventDefault();
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value,
-    });
-  };
+  // // @INCOMPLETE remove this before final version
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:3001/api/users")
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, [load]);
 
   const acceptHandler = (e) => {
     e.preventDefault();
     setUser({
-      ...user,
-      opt_in: true,
+        ...user,
+        opt_in: true,
     });
-  };
+};
 
-  const declineHandler = (e) => {
+const declineHandler = (e) => {
     e.preventDefault();
     setUser({
-      ...user,
-      opt_in: false,
+        ...user,
+        opt_in: false,
     });
-  };
+};
 
-  return (
-    <div>
+return (
+  <div>
       <Container>
-        <div className="registrationForm">
-          <Form onSubmit={submitUser}>
-            <div>
-              <Consent accept={acceptHandler} decline={declineHandler} />
-            </div>
-            <div className="registrationForm">
-              <Row className="mb-3">
-                <Form.Group
-                  as={Col}
-                  className="mb-3 mt-3"
-                  controlId="formBasicFirstName"
-                >
-                  <Form.Label>First Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="first_name"
-                    value={user.first_name}
-                    onChange={changeHandler}
-                    placeholder="Enter first name"
-                  />
-                </Form.Group>
-                <Form.Group
-                  as={Col}
-                  className="mb-3 mt-3"
-                  controlId="formBasicLastName"
-                >
-                  <Form.Label>Last Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="last_name"
-                    value={user.last_name}
-                    onChange={changeHandler}
-                    placeholder="Enter last name"
-                  />
-                </Form.Group>
-              </Row>
-              <Form.Group className="mb-3 mt-3" controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  name="email"
-                  value={user.email}
-                  onChange={changeHandler}
-                  placeholder="Enter email"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3 mt-3" controlId="formBasicUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="username"
-                  value={user.username}
-                  onChange={changeHandler}
-                  placeholder="Enter username"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3 mt-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  value={user.password}
-                  onChange={changeHandler}
-                  placeholder="Enter password"
-                />
-              </Form.Group>
-              <Row className="mb-3">
-                <Form.Group
-                  as={Col}
-                  className="mb-3 mt-3"
-                  controlId="formBasicLanguage"
-                >
-                  <Form.Label>Language</Form.Label>
-                  <Form.Control
-                    as="select"
-                    name="language"
-                    onChange={changeHandler}
-                    aria-label="Language"
-                  >
-                    <option>Select Language</option>
-                    <option value="French">French</option>
-                    <option value="Spanish">Spanish</option>
-                    <option value="Japanese">Japanese</option>
-                  </Form.Control>
-                </Form.Group>
-                <Form.Group
-                  as={Col}
-                  className="mb-3 mt-3"
-                  controlId="formBasicCourse"
-                >
-                  <Form.Label>Course</Form.Label>
-                  <Form.Control
-                    as="select"
-                    name="course"
-                    onChange={changeHandler}
-                    aria-label="Course"
-                  >
-                    <option>Select Course</option>
-                    <option value="French 101">French 101</option>
-                    <option value="Spanish 101">Spanish 101</option>
-                    <option value="Japanese 101">Japanese 101</option>
-                  </Form.Control>
-                </Form.Group>
-                <Form.Group
-                  as={Col}
-                  className="mb-3 mt-3"
-                  controlId="formBasicInstructor"
-                >
-                  <Form.Label>Instructor</Form.Label>
-                  <Form.Control
-                    as="select"
-                    name="instructor"
-                    onChange={changeHandler}
-                    aria-label="Instructor"
-                  >
-                    <option>Select Instructor</option>
-                    <option value="French Instructor">French Instructor</option>
-                    <option value="Spanish Instructor">
-                      Spanish Instructor
-                    </option>
-                    <option value="Japanese Instructor">
-                      Japanese Instructor
-                    </option>
-                  </Form.Control>
-                </Form.Group>
-              </Row>
-              <Button type="submit">Sign Up</Button>
-              <p>
-                Already Registered?{" "}
-                <a
-                  onClick={() => {
-                    changeScreen("login");
-                  }}
-                >
-                  Login
-                </a>
-              </p>
-              {/* <Button
-              onClick={() => {
-                changeScreen("login");
-              }}
-            >
-              Login
-            </Button> */}
-            </div>
-          </Form>
-        </div>
+          <div className="registrationForm">
+              <div className="Consent">
+                  <div className="register-content">
+                      {
+                          !actionTakenOnConsent ? <Consent setActionTakenOnConsent={setActionTakenOnConsent}/>
+                              :
+                              <FormForRegistration setUserLogged={setUserLogged} userLogged={userLogged}
+                                                   user={user}
+                                                   setUser={setUser} changeScreen={changeScreen}/>
+                      }
+                  </div>
+              </div>
+          </div>
       </Container>
-    </div>
-  );
+  </div>
+);
 };
 
 export default Register;
+
+  // const submitUser = (e) => {
+  //   e.preventDefault();
+  //   console.log(user);
+  //   axios
+  //     .post("http://localhost:3001/api/users", user)
+  //     .then((res) => {
+  //       setUser({
+  //         first_name: "",
+  //         last_name: "",
+  //         username: "",
+  //         email: "",
+  //         language: "",
+  //         course: "",
+  //         instructor: "",
+  //         password: "",
+  //         opt_in: false,
+  //       });
+  //       console.log(res);
+  //       localStorage.setItem("user", JSON.stringify(res.data.user.id));
+  //       userLogged === 0 ? setUserLogged(1) : setUserLogged(0);
+  //       load === 0 ? setLoad(1) : setLoad(0);
+  //       window.location.replace("https://ling.towson.edu/");
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       load === 0 ? setLoad(1) : setLoad(0);
+  //     });
+  // };
+
+  // const changeHandler = (e) => {
+  //   e.preventDefault();
+  //   setUser({
+  //     ...user,
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
+
+  // const acceptHandler = (e) => {
+  //   e.preventDefault();
+  //   setUser({
+  //     ...user,
+  //     opt_in: true,
+  //   });
+  // };
+
+  // const declineHandler = (e) => {
+  //   e.preventDefault();
+  //   setUser({
+  //     ...user,
+  //     opt_in: false,
+  //   });
+  // };
+
+//   return (
+//     <div>
+//       <Container>
+//         <div className="registrationForm">
+//           <Form onSubmit={submitUser}>
+//             <div>
+//               <Consent accept={acceptHandler} decline={declineHandler} />
+//             </div>
+//             <div className="registrationForm">
+//               <Row className="mb-3">
+//                 <Form.Group
+//                   as={Col}
+//                   className="mb-3 mt-3"
+//                   controlId="formBasicFirstName"
+//                 >
+//                   <Form.Label>First Name</Form.Label>
+//                   <Form.Control
+//                     type="text"
+//                     name="first_name"
+//                     value={user.first_name}
+//                     onChange={changeHandler}
+//                     placeholder="Enter first name"
+//                   />
+//                 </Form.Group>
+//                 <Form.Group
+//                   as={Col}
+//                   className="mb-3 mt-3"
+//                   controlId="formBasicLastName"
+//                 >
+//                   <Form.Label>Last Name</Form.Label>
+//                   <Form.Control
+//                     type="text"
+//                     name="last_name"
+//                     value={user.last_name}
+//                     onChange={changeHandler}
+//                     placeholder="Enter last name"
+//                   />
+//                 </Form.Group>
+//               </Row>
+//               <Form.Group className="mb-3 mt-3" controlId="formBasicEmail">
+//                 <Form.Label>Email</Form.Label>
+//                 <Form.Control
+//                   type="email"
+//                   name="email"
+//                   value={user.email}
+//                   onChange={changeHandler}
+//                   placeholder="Enter email"
+//                 />
+//               </Form.Group>
+//               <Form.Group className="mb-3 mt-3" controlId="formBasicUsername">
+//                 <Form.Label>Username</Form.Label>
+//                 <Form.Control
+//                   type="text"
+//                   name="username"
+//                   value={user.username}
+//                   onChange={changeHandler}
+//                   placeholder="Enter username"
+//                 />
+//               </Form.Group>
+//               <Form.Group className="mb-3 mt-3" controlId="formBasicPassword">
+//                 <Form.Label>Password</Form.Label>
+//                 <Form.Control
+//                   type="password"
+//                   name="password"
+//                   value={user.password}
+//                   onChange={changeHandler}
+//                   placeholder="Enter password"
+//                 />
+//               </Form.Group>
+//               <Row className="mb-3">
+//                 <Form.Group
+//                   as={Col}
+//                   className="mb-3 mt-3"
+//                   controlId="formBasicLanguage"
+//                 >
+//                   <Form.Label>Language</Form.Label>
+//                   <Form.Control
+//                     as="select"
+//                     name="language"
+//                     onChange={changeHandler}
+//                     aria-label="Language"
+//                   >
+//                     <option>Select Language</option>
+//                     <option value="French">French</option>
+//                     <option value="Spanish">Spanish</option>
+//                     <option value="Japanese">Japanese</option>
+//                   </Form.Control>
+//                 </Form.Group>
+//                 <Form.Group
+//                   as={Col}
+//                   className="mb-3 mt-3"
+//                   controlId="formBasicCourse"
+//                 >
+//                   <Form.Label>Course</Form.Label>
+//                   <Form.Control
+//                     as="select"
+//                     name="course"
+//                     onChange={changeHandler}
+//                     aria-label="Course"
+//                   >
+//                     <option>Select Course</option>
+//                     <option value="French 101">French 101</option>
+//                     <option value="Spanish 101">Spanish 101</option>
+//                     <option value="Japanese 101">Japanese 101</option>
+//                   </Form.Control>
+//                 </Form.Group>
+//                 <Form.Group
+//                   as={Col}
+//                   className="mb-3 mt-3"
+//                   controlId="formBasicInstructor"
+//                 >
+//                   <Form.Label>Instructor</Form.Label>
+//                   <Form.Control
+//                     as="select"
+//                     name="instructor"
+//                     onChange={changeHandler}
+//                     aria-label="Instructor"
+//                   >
+//                     <option>Select Instructor</option>
+//                     <option value="French Instructor">French Instructor</option>
+//                     <option value="Spanish Instructor">
+//                       Spanish Instructor
+//                     </option>
+//                     <option value="Japanese Instructor">
+//                       Japanese Instructor
+//                     </option>
+//                   </Form.Control>
+//                 </Form.Group>
+//               </Row>
+//               <Button type="submit">Sign Up</Button>
+//               <p>
+//                 Already Registered?{" "}
+//                 <a
+//                   onClick={() => {
+//                     changeScreen("login");
+//                   }}
+//                 >
+//                   Login
+//                 </a>
+//               </p>
+//               {/* <Button
+//               onClick={() => {
+//                 changeScreen("login");
+//               }}
+//             >
+//               Login
+//             </Button> */}
+//             </div>
+//           </Form>
+//         </div>
+//       </Container>
+//     </div>
+//   );
+// };
+
+// export default Register;
